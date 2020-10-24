1051b652c3af9729641de10bf55e2c6dbf7b9c6c
Folorunsho shina <58972665+yungxix@users.noreply.github.com>
2020-10-24 15:36:04 +0100

    Add files via upload

---
+++ Stock/hello.js
@@ -0,0 +1,43 @@
+let name='Arial winks';
+let age='12';
+console.log(name);
+console.log(age);
+
+let option="true";
+let height;
+
+console.log(option);
+console.log(height);
+let eyecolour=null;
+
+let shopping=['pen', 'Noodles','plaintain'];
+
+
+
+let number=[];
+numbers=[1,2,1,2,3,2,3,1];
+if (number[0]==number[3]){
+    console.log('correct');
+}
+
+if (numbers[0]==numbers[4]) {
+    console.log('correct');
+
+}
+else{
+    console.log('input error, try again');
+}
+
+
+if (numbers[0]==numbers[2]&& numbers[2]==numbers[6]) {
+    console.log("valid");
+}
+else{
+    console.log('error, try again')
+}
+
+let games='Football';
+switch(games){
+    case "soccer":
+        console.log("I hate Soccer");
+}
